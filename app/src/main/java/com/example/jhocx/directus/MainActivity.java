ac908cc9bc59a5b93926f30a4963cf8b9946c21a
LAPTOP-3IPPG5K7\Jhocx <jhocx@u.nus.edu>
2018-07-05 18:51:42 +0800

    Main Activity toolbar added. Font file added to assets folder.

--- app/src/main/java/com/example/jhocx/directus/MainActivity.java
+++ app/src/main/java/com/example/jhocx/directus/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.jhocx.directus;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t = (TextView) findViewById(R.id.nearbyMalls);
+        Typeface myCustomFont = Typeface.createFromAsset(getAssets(),"fonts/Roboto-Light.ttf");
+        t.setTypeface(myCustomFont);
     }
 }
